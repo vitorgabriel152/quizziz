c490f8ae48bfded7b3a1dd7023b4c9a57897d273
Fredinzin <141040160+vitorgabriel152@users.noreply.github.com>
2025-09-01 11:46:43 -0300

    Update and rename vite.confi.js to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/quizizz/', // Nome do seu repositório no GitHub
+  base: '/quizziz/', // Nome do seu repositório no GitHub
 })
