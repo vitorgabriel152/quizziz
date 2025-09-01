c08376720e5740b1a629d2e3b33e734db009a10d
Fredinzin <141040160+vitorgabriel152@users.noreply.github.com>
2025-09-01 11:43:56 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import Home from './pages/Home'
+import Play from './pages/Play'
+import Results from './pages/Results'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'play', element: <Play /> },
+      { path: 'results', element: <Results /> },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
